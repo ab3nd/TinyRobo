0ca1ee6adefd91f835f4dc341acc91996dc1b7e0
Abe Shultz <orphrey@gmail.com>
2016-05-13 16:56:57 -0400

    Fixed bad package name in catkin-related files

--- software/catkin_ws/src/sim_robots/src/sim_robots/sim_robot.cpp
+++ software/catkin_ws/src/sim_robots/src/sim_robots/sim_robot.cpp
@@ -1,1 +1,24 @@
 #include <sim_robots/sim_robot.hpp>
+
+SimRobot::timeCallback(const std_msgs::Header::ConstPtr& msg)
+{
+	ROS_INFO(msg->stamp);
+}
+
+SimRobot::motorCallback()
+{
+	//Do nothing for the moment
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "sim_robot");
+	ros::NodeHandle node("~");
+
+	SimRobot sr();
+
+	//Listen for updates from the world
+	clockSub = node.subscribe("sim_world_clock", 1, &SimRobot::timeCallback, &sr);
+
+	//TODO Listen for motor velocity commands
+}
---
+++ software/catkin_ws/src/sim_robots/src/sim_robots/sim_world.cpp
@@ -0,0 +1,40 @@
+#include <sim_robots/sim_world.hpp>
+
+SimWorld::SimWorld()
+{
+	//Set up the publisher
+
+}
+SimWorld::step()
+{
+	/* Publish a clock message, which is just a ROS Header.
+	 * The header automatically has a timestamp, which is what the sim robots
+	 * need to update themselves.
+	 */
+	std_msgs::Header msg;
+	worldClock.publish(msg);
+
+}
+
+SimWorld::update()
+{
+	//For now, do nothing
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "sim_world");
+	ros::NodeHandle node("~");
+
+	SimWorld world();
+
+	//TODO Subscribe to all robots updates
+
+	ros::Rate r(100); //Update frequency for the world, in Hz
+	while(ros::ok())
+	{
+		world.update();
+		r.sleep();
+	}
+
+}
