18d0c5c83f01d7f7bfa370c76ee31d1d83d3a713
ams <orphrey@gmail.com>
2016-06-09 12:22:40 +0200

    fiddling with printed instructions

--- software/tuio_test.py
+++ software/tuio_test.py
@@ -5,12 +5,21 @@
 #it's a good start for remote controlling the robots. 
 
 import tuio
+import time
 trkr = tuio.Tracking()
+
+threshold = 5
 
 try: 
 	while True: 
 		trkr.update() 
 		for obj in trkr.objects(): 
-			print obj 
+			if obj.angle > threshold or obj.angle < 360 - threshold:
+				if obj.angle < 180:
+					print "Turn {0} left {1} degrees".format(obj.id, 360-obj.angle)
+				else:
+					print "Turn {0} right {1} degrees".format(obj.id, obj.angle)
+
+
 except KeyboardInterrupt: 
 	trkr.stop() 
