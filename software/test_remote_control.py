02820a3553982f5cebde57ae855272af31bc8d7d
ams <orphrey@gmail.com>
2016-06-15 15:02:07 +0200

    Added a new script for driving a single robot with a known IP.

---
+++ software/test_remote_control.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Stupid remote control for robots from a pygame interface. 
+# The interface provides connection control, l/r arrows are one motor, 
+# up/down are the other motor, spacebar to stop. 
+
+import Tkinter as Tk
+
+if __name__ == "__main__":
+	root = TK()
+	w = Tk.Label(root, text="O SHIT WADDUP")
+	w.pack()
+	root.mainloop()
+	
