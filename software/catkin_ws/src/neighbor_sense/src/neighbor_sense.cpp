ab3ce9236748e439673d405c70ed8cbed8ebb30e
ams <orphrey@gmail.com>
2016-10-23 15:48:37 -0400

    Calculates the distances now

--- software/catkin_ws/src/neighbor_sense/src/neighbor_sense.cpp
+++ software/catkin_ws/src/neighbor_sense/src/neighbor_sense.cpp
@@ -12,7 +12,7 @@ void NeighborSense::tagCallback(const apriltags_ros::AprilTagDetectionArray::Con
 {
 	ROS_WARN("Tag callback called");
 	//For each visible tag
-	for(std::vector<apriltags_ros::AprilTagDetection>::const_iterator tag = tagArray->detections.begin(); tag != tagArray->detections.end(); ++tag)
+	for(std::vector<apriltags_ros::AprilTagDetection>::const_iterator tag = tagArray->detections.begin(); tag != tagArray->detections.end(); tag++)
 	{
 		//If a publisher for that tag does not exist
 		if(robotSensors.count((*tag).id) == 0)
@@ -26,9 +26,19 @@ void NeighborSense::tagCallback(const apriltags_ros::AprilTagDetectionArray::Con
 		}
 
 		//Publish the distances from this tag to all others
-		std_msgs::String msg;
-		msg.data = "LOL test";
-		robotSensors[(*tag).id].publish(msg);
+		for(std::vector<apriltags_ros::AprilTagDetection>::const_iterator otherTag = tagArray->detections.begin(); otherTag != tagArray->detections.end(); otherTag++)
+		{
+			float thisX = (*tag).pose.pose.position.x;
+			float thisY = (*tag).pose.pose.position.y;
+			float otherX = (*otherTag).pose.pose.position.x;
+			float otherY = (*otherTag).pose.pose.position.y;
+			float distance = sqrt(pow((thisX - otherX),2) + pow((thisY - otherY),2));
+			std_msgs::String msg;
+			std::stringstream debug;
+			debug << (*tag).id << " -> " << (*otherTag).id << " : " << distance;
+			msg.data = debug.str();
+			robotSensors[(*tag).id].publish(msg);
+		}
 	}
 
 }
