a0bae4c635cf7534124d77d81771f33d558d5143
ams <orphrey@gmail.com>
2016-10-24 15:48:32 -0400

    Added bearing to nearby robots, but it needs debugging

--- software/catkin_ws/src/neighbor_sense/src/neighbor_sense.cpp
+++ software/catkin_ws/src/neighbor_sense/src/neighbor_sense.cpp
@@ -28,14 +28,27 @@ void NeighborSense::tagCallback(const apriltags_ros::AprilTagDetectionArray::Con
 		//Publish the distances from this tag to all others
 		for(std::vector<apriltags_ros::AprilTagDetection>::const_iterator otherTag = tagArray->detections.begin(); otherTag != tagArray->detections.end(); otherTag++)
 		{
+			//Euclidian distance in 2D. Could extend to 3D, but tag occlusion might become an issue.
 			float thisX = (*tag).pose.pose.position.x;
 			float thisY = (*tag).pose.pose.position.y;
 			float otherX = (*otherTag).pose.pose.position.x;
 			float otherY = (*otherTag).pose.pose.position.y;
 			float distance = sqrt(pow((thisX - otherX),2) + pow((thisY - otherY),2));
+
+			//Get the rotation in the XY (yaw), YZ (roll), and ZX (pitch) planes
+			//For robots on the table, roll and pitch should be quite small
+			float thisZ = (*tag).pose.pose.position.z;
+			float otherZ = (*otherTag).pose.pose.position.z;
+			float yaw = atan2((thisY-otherY),(thisX-otherX));
+			float roll = atan2((thisZ-otherZ), (thisY-otherY));
+			float pitch = atan2((thisX-otherX), (thisZ-otherZ));
+
+			//Create a quaternion from rotation vectors
+
+			//Text message for debug reasons, convert to something else when it's working
 			std_msgs::String msg;
 			std::stringstream debug;
-			debug << (*tag).id << " -> " << (*otherTag).id << " : " << distance;
+			debug << (*tag).id << " -> " << (*otherTag).id << " : " << distance << std::endl << roll << ", " << pitch << ", " << yaw;
 			msg.data = debug.str();
 			robotSensors[(*tag).id].publish(msg);
 		}
