9e8a73cc36727b27bb64613d4cede30ea10f4889
ams <orphrey@gmail.com>
2016-12-28 14:55:22 -0500

    Oracle for range & bearing, now draws robots as orientable polygons and gives them orientation

--- software/test_coord.py
+++ software/test_coord.py
@@ -6,27 +6,28 @@ import copy
 from pprint import pprint
 
 class cSpaceMember:
-    def __init__(self, id):
+    def __init__(self, id, oracle):
+        #Don't have a world location when starting up
         self.x = None
         self.y = None
+        #Don't have a heading when starting up
+        self.theta = None
         self.id = id
-        self.neighbors = None
+        #self.neighbors = None
         self.state = {}
         self.isBeacon = False
         self.actions = {}
         #dictionary of dictionaries, first dictionary indexed by 
         #beacon id, next by heard-from, entry is distance
         self.beacons = []
+        self.unchangedCount = 0
+        self.oracle = oracle
         
-    def setNeighbors(self, neighbors):
-        self.neighbors = neighbors
-    
     def neighborDistance(self, targetID):
-        for neighbor, dist in self.neighbors:
-            #print "{0} --> {1}, {2}".format(self.id, neighbor.id, dist)
-            if neighbor.id == targetID:
-                return dist
-        raise ValueError
+        if targetID in self.oracle.getNeighbors(self.id):
+            return self.oracle.getDistance(self.id, targetID)
+        else:
+            raise ValueError
     
     def becomeBeacon(self):
         self.isBeacon = True
@@ -34,11 +35,11 @@ class cSpaceMember:
         self.beacons.append([self.id, [self.id], 0])
         
     def broadcastState(self):
-        for neighbor in self.neighbors:
+        for neighbor in self.oracle.getNeighbors(self.id):
             #Send a beacon message for each of self.beacons
             for beacon in self.beacons:
                 #The other end gets their own copy of the message, not references to mine
-                neighbor[0].sendBeaconMessage(copy.deepcopy(beacon), self.id)
+                space[neighbor].sendBeaconMessage(copy.deepcopy(beacon), self.id)
         
     
     def sendBeaconMessage(self, beaconMessage, fromID):
@@ -49,15 +50,16 @@ class cSpaceMember:
         #Check if it's shorter than any other known routes to that beacon
         newRoutes = []
         noMatch = True
-        change = False
         for route in self.beacons:
             if route[0] == newBeaconMessage[0]:
-                #It's a route to the same beacon
+                #It's a route to the same beacon#pprint(self.beacons)
                 noMatch = False
                 if route[2] > newBeaconMessage[2]:
                     #The existing route was longer, so keep this one
                     print "{2} replacing {0} with {1}".format(route, newBeaconMessage, self.id)
-                    newRoutes.append(newBeaconMessage)                    
+                    newRoutes.append(newBeaconMessage)
+                    #We changed the routes, so the unchanged count drops back to zero
+                    self.unchangedCount = 0 
                 else:
                     #Shorter route, keep the old one
                     newRoutes.append(route)
@@ -68,24 +70,45 @@ class cSpaceMember:
             #The route under consideration is to a previously unheardof beacon, so keep it
             #print "{1} adding new route: {0}".format(newBeaconMessage, self.id)
             newRoutes.append(newBeaconMessage)
+            #We changed the routes, so the unchanged count drops back to zero
+            self.unchangedCount = 0 
         #Update
         self.beacons = newRoutes
-        #pprint(self.beacons)
         
+    def updateCount(self):
+        self.unchangedCount += 1
+        print "{1} updates since last change: {0}".format(self.unchangedCount, self.id) 
         
+    def createCoordSys(self):
+        #This robot is at the origin of the system
+        self.x = 0
+        self.y = 0
+        #This robot is facing straight forward
+        self.theta = 0
+        
+        #Tell all my neighbors their positions
+    
     def update(self):
         #This is where we put rules. Rules are composed of a boolean condition, 
         #a thing to do if the boolean condition is true, and a rate to do it at
         if not self.isBeacon and len(self.beacons) == 0:
             #Not a beacon, and haven't heard of any beacons
-            #Add the action of becoming a beacon to the actions with a very low probability
-            self.actions["becomeBeacon"] = (self.becomeBeacon, 0.001)
+            #Add the action of becoming a beacon to the actions with a low probability
+            self.actions["becomeBeacon"] = (self.becomeBeacon, 0.01)
         if not self.isBeacon and len(self.beacons) > 0:
             # Not a beacon, but have heard of beacons, so don't try to become a beacon. 
             pass
         if True:
             #Always broadcast state
             self.actions["sendUpdates"] = (self.broadcastState, 1.0)
+        
+        #If I am a beacon, or have seen beacons, start counting updates since the last change
+        if self.isBeacon or len(self.beacons) > 0:
+            self.actions["countStable"] = (self.updateCount , 1.0)
+        
+        #There's probably a heuristic for what this threshold should be
+        if self.isBeacon and self.unchangedCount > 100:
+            self.actions["createCoords"] = (self.createCoordSys, 1.0)
             
         # Run (or don't) all the actions according to their probability
         for action in self.actions.keys():
@@ -95,33 +118,99 @@ class cSpaceMember:
                 self.actions[action][0]()
             else:
                 del self.actions[action]
+
+class worldPosition:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.theta = 0
+                
+class RaBOracle:
+       
+    def __init__(self):
+        self.spaceSize = 1000
+        self.pairwiseDist = {}
+        self.pairwiseBearing = {}
+        self.neighbors = {}
+        self.realWorld = {}   
+    
+    def add(self, newID):
+        #Tie a (random) world position to an ID
+        wp = worldPosition()
+        wp.x = random.random() * self.spaceSize
+        wp.y = random.random() * self.spaceSize
+        wp.theta = random.random() * 2 * math.pi
+        self.realWorld[newID] = wp
         
+        #New node, so don't know neighbors yet
+        self.neighbors[newID] = []
         
-        
-#Euclidian distance
-def distance(x1, y1, x2, y2):
-    return math.sqrt(pow((x1-x2), 2) + pow((y1 - y2), 2))
+        #Update the neighbors, distances, and bearings
+        #This will include self-distance
+        for otherID in self.realWorld.keys():
+            #Distancese first
+            dist = self.distance(wp, self.realWorld[otherID])
+            #Distance is the same in both directions
+            self.pairwiseDist[(otherID, newID)] = self.pairwiseDist[(newID, otherID)] = dist
+            
+            #Neighborhood is based on distance threshold
+            if dist < 200:
+                #Neighborhood goes both ways
+                self.neighbors[newID].append(otherID)
+                self.neighbors[otherID].append(newID)
+            
+            #Bearing is a little more complicated, but this is essentially the angle that the
+            #line from the origin to the point would be, if the starting point were the origin
+            #That is, treat the first robot as the origin, and calculate atan2 for the difference
+            #in the position of the second robot from the first one
+            dx = wp.x - self.realWorld[otherID].x
+            dy = wp.y - self.realWorld[otherID].y
+            self.pairwiseBearing[(newID, otherID)] = math.atan2(dy, dx) + wp.theta
+            #Of course, the other robot sees the difference the other way around, 
+            #so negate the signs and use the other robot's heading 
+            dx *= -1
+            dy *= -1
+            self.pairwiseBearing[(otherID, newID)] = math.atan2(dy, dx) + self.realWorld[otherID].theta
+            
+    #Euclidian distance between two world positions
+    def distance(self, wp1, wp2):
+        return math.sqrt(pow((wp1.x-wp2.x), 2) + pow((wp1.y - wp2.y), 2))
+
+    def getDistance(self, id1, id2):
+        return self.pairwiseDist[(id1, id2)]
+    
+    def getBearing(self, id1, id2):
+        return self.pairwiseBearing[(id1, id2)]    
+    
+    def getNeighbors(self, id):
+        return self.neighbors[id]
+    
+    def getPosition(self, id):
+        return self.realWorld[id]
 
 class renderer:
     def __init__(self):
         self.count = 0
         
-    def render(self, space):
+    def render(self, space, oracle):
         with open("frame_{0}.dot".format(self.count), "w") as outFile:
             outFile.write("strict digraph space{\n")
-            for xPos, yPos, member in space:
-                outFile.write("member_{0}[\n".format(member.id))
-                outFile.write("   label={0}\n".format(member.id))
-                if member.isBeacon:
-                    outFile.write("   shape=doublecircle\n")
+            for id in space.keys():
+                outFile.write("member_{0}[\n".format(id))
+                outFile.write("   label={0}\n".format(id))
+                outFile.write("   orientation={0}\n".format(oracle.getPosition(id).theta * 180/math.pi))
+                if space[id].isBeacon:
+                    outFile.write("   shape=house\n")
                     outFile.write("   color=red\n")
                 else:
-                    outFile.write("   shape=circle\n")
-                outFile.write("   pos=\"{0},{1}!\"\n".format(xPos, yPos))
+                    outFile.write("   shape=house\n")
+                #Set the position 
+                pos = oracle.getPosition(id)
+                outFile.write("   pos=\"{0},{1}!\"\n".format(pos.x, pos.y))
                 outFile.write("]\n")
                 
                 
-                for beacon in member.beacons:
+                for beacon in space[id].beacons:
                     #Generate the dot links for each route.
                     nodes = [str(x) for x in reversed(beacon[1])]
                     for ii in range(len(nodes)-1):
@@ -135,28 +224,22 @@ if __name__ == "__main__":
     random.seed()
     myRend = renderer()
     
-    #Create a bunch of cSpaceMembers in the space
-    space = []
-    members = 30
-    spaceSize = 1000
-    for ii in range(members):
-        space.append((spaceSize * random.random(), spaceSize * random.random(), cSpaceMember(ii)))
-        
-    #Set up the neighbors based on distance
-    for fromMember in space:
-        neighbors = []
-        for toMember in space:
-            d = distance(fromMember[0], fromMember[1], toMember[0], toMember[1])
-            if d < 300:
-                neighbors.append((toMember[2], d))
-        fromMember[2].setNeighbors(neighbors)
-        print "{0} has {1} neighbors".format(fromMember[2].id, len(neighbors)) 
+    #Create a bunch of cSpaceMembers and set up the space
+    oracle = RaBOracle()
+    space = {}
     
+    members = 30
+    for ii in range(members):
+        oracle.add(ii)
+        #Member with a unique ID
+        member = cSpaceMember(ii, oracle)
+        space[ii] = member
+            
     while True:
-        for member in space:
+        for id in space.keys():
             #print "-- {0} --".format(member[2].id)
-            member[2].update()
-        myRend.render(space)
+            space[id].update()
+        myRend.render(space, oracle)
         
         
             
