295744763f790ae91a78ce52c636893cf6be8a9d
Abe Shultz <orphrey@gmail.com>
2017-04-06 15:18:38 -0400

    Basic kivy app, from the kivy docs

---
+++ software/ui_test/ui_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#Display JPGs and log user interactions 
+
+import kivy
+kivy.require('1.9.1') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
