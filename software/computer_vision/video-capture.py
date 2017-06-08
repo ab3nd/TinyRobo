1e143387add4aa5e20d6d10655d3378ab3d8ef8a
Joshua Stone <joshua.gage.stone@gmail.com>
2017-06-08 16:51:28 -0400

    Test line detection in camera feedback

--- software/computer_vision/video-capture.py
+++ software/computer_vision/video-capture.py
@@ -1,12 +1,13 @@
 from cv2 import VideoCapture, cvtColor, imshow, COLOR_BGR2GRAY, waitKey, destroyAllWindows
 
-camera = VideoCapture()
+from lib.bluedetection import *
+camera = VideoCapture(0)
 
 while(True):
-        ret, frame = camera.read()
-  
-        if frame:
-            gray = cvtColor(frame, COLOR_BGR2GRAY)
+        ret, frame = camera.read() 
+ 
+        if ret:
+            gray = find_lines(frame)
             imshow('frame', gray)
             if waitKey(1) & 0xFF == ord('q'):
                 break
