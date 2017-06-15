9077a350bad9883faef749e9de74e71049c05a20
Joshua Stone <joshua.gage.stone@gmail.com>
2017-06-15 14:48:03 -0400

    Cull contours more aggressively so that scanning at each angle isn't as expensive

--- software/computer_vision/lib/laserscan.py
+++ software/computer_vision/lib/laserscan.py
@@ -1,6 +1,6 @@
-from numpy import cos, sin, pi, deg2rad
+from numpy import cos, sin, pi, deg2rad, hypot, degrees, array
 from cv2 import pointPolygonTest
-from math import hypot, atan2, degrees
+from math import atan2
 
 def calc_coordinates(x, y, angle, distance):
     xcoord = x + distance * cos(deg2rad(angle))
@@ -12,6 +12,9 @@ def calc_coordinates(x, y, angle, distance):
 def calc_angle(x1, y1, x2, y2):
     return degrees(atan2(y1 - y2, x2 - x1))
 
+def calc_distance(x1, y1, x2, y2):
+    return hypot(x2 - x1, y1 - y2)
+
 def is_within_contour(contours, coordinates):
     for contour in contours:
         test = pointPolygonTest(contour, coordinates, False)
@@ -19,15 +22,17 @@ def is_within_contour(contours, coordinates):
             return True
     return False
 
-def contour_ranges(x1, y1, contours):
+def contour_ranges(x1, y1, contours, max_distance):
     ranges = []
     for contour in contours:
-        angle_values = []
-        for points in contour:
-            x2, y2 = points[0][0], points[0][1]
-            angle_values.append(calc_angle(x1, y1, x2, y2))
-            range_min, range_max = min(angle_values), max(angle_values)
-        ranges.append((range_min, range_max, contour))
+        points = [points[0] for points in contour]
+
+        coordinates = array([(x2, y2) for x2, y2 in points if calc_distance(x2, x1, y1, y2) <= max_distance])
+
+        if coordinates.any():
+            angles = [calc_angle(x1, y1, x2, y2) for x2, y2 in coordinates]
+            range_min, range_max = min(angles), max(angles)
+            ranges.append((range_min, range_max, coordinates))
 
     return ranges
 
@@ -36,13 +41,14 @@ def filter_contours(contours, angle):
     for angle_min, angle_max, contour in contours:
         if angle_min <= angle <= angle_max:
             results.append(contour)
+        
     return results
 
-def line_intersections(x, y, contours, min_range, max_range, interval):
+def line_intersections(x, y, contours, min_angle=0, max_angle=180, angle_interval=1):
     points = []
-    for angle in range(min_range, max_range + 1, 5):
+    for angle in range(min_angle, max_angle + 1, angle_interval):
         c = filter_contours(contours, angle)
-        for point in range(1, x, 10):
+        for point in range(1, x, 5):
             x2, y2 = calc_coordinates(x, y, angle, point)
             if is_within_contour(c, (x2, y2)):
                 points.append((x2, y2))
