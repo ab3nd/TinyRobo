9a400c5f40aca64d3a587d4cd3f598992d954472
ams <orphrey@gmail.com>
2017-06-28 19:09:31 -0400

    Calculates laser scan scan_time and time_increment based on ROS wall clock time, so hopefully interpolation will work

--- software/catkin_ws/src/laser_oracle/src/laser_client.py
+++ software/catkin_ws/src/laser_oracle/src/laser_client.py
@@ -23,14 +23,31 @@ def LaserDriver():
 	#TODO make this a parameter
 	rate = rospy.Rate(3)
 
+	lastScan = None
+
 	while not rospy.is_shutdown():
 		rospy.wait_for_service("laser_oracle")
 		#Try the service call
 		try:
 			laserMsg = rospy.ServiceProxy("laser_oracle", LaserOracle)
 			response = laserMsg(robotID = id, angleMin = angleMin, angleMax = angleMax, angleIncrement = angleIncrement, rangeMin = rangeMin, rangeMax = rangeMax)
-			#TODO calculate and update the scanTime and time_increment
+
+			#Calculate and update the scanTime and time_increment
+			if lastScan is not None:
+				deltaT = rospy.Time.now() - lastScan
+				#Write the time between scans to the response laserScan object
+				response.laserScan.scan_time = deltaT.to_sec()
+
+				#This does rather assume that we start each scan immedately
+				#After the previous one, and the time is divided equally
+				scanCount = len(response.laserScan.ranges)
+				if scanCount != 0: #Avoid div/0
+					response.laserScan.time_increment = deltaT.to_sec()/scanCount
+
 			pub.publish(response.laserScan)
+			#Update time last message was published
+			lastScan = rospy.Time.now()
+
 		except rospy.ServiceException, e:
 			print "Service call failed: {0}".format(e)
 
