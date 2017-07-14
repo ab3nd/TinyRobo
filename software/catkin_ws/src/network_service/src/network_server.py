fa3a2b40951074d93261e998d7426ed66ae785c0
ams <orphrey@gmail.com>
2017-07-14 17:34:01 -0400

    Range limiting works, next to add is some idea of link quality

--- software/catkin_ws/src/network_service/src/network_server.py
+++ software/catkin_ws/src/network_service/src/network_server.py
@@ -12,22 +12,45 @@
 
 import rospy
 from math import pow, sqrt
-from net_message.srv import *
-from net_message.msg import *
+from network_service.srv import *
+from network_service.msg import *
+from distance_oracle.srv import *
 from apriltags_ros.msg import *
 
 class NetworkServer():
 
-	def __init__(self):
+	def __init__(self, maxTxRange):
 		self.currentTags = {}
+		if(maxTxRange > 0):
+			#Distance service needed to determine reachability based on range
+			self.maxTxRange = maxTxRange
+			rospy.wait_for_service('distance_oracle')
+			self.getDistance = rospy.ServiceProxy('distance_oracle', DistanceOracle)
+		else:
+			self.getDistance = None
+
+	def is_reachable(self, toID, fromID):
+		#We don't care about distance, none was set
+		if self.getDistance == None:
+			rospy.logwarn("Always sending")
+			return True
+		else:
+			#request distance from distance oracle and check that we're not over it
+			distance = self.getDistance(toID, fromID)
+			#rospy.logwarn("{0}, {1}".format(distance, self.maxTxRange))
+			if distance.distance > self.maxTxRange:
+				return False
+			return True
 
 	def handle_msg_req(self, req):
 		if req.robotID in self.currentTags.keys():
-			#We can see that robot, so publish a message to it
-			msg = net_message()
-			msg.content = req.content
-			self.currentTags[int(req.robotID)].publish(msg)
-			return net_messageResponse()
+			if self.is_reachable(req.robotID, req.fromID):
+				#We can reach that robot over the network, so publish a message to it
+				msg = NetMsgToRobot()
+				msg.content = req.content
+				msg.fromID = req.fromID
+				self.currentTags[int(req.robotID)].publish(msg)
+		return NetMsgResponse()
 
 	def update_tags(self, msg):
 		seenTags = [int(tag.id) for tag in msg.detections]
@@ -40,21 +63,26 @@ class NetworkServer():
 			if botID in self.currentTags.keys():
 				continue
 			else:
-				currentTags[botID] = rospy.Publisher("/messages_to_{0}".format(botID), net_message, 1)
+				self.currentTags[botID] = rospy.Publisher("/messages_to_{0}".format(botID), NetMsgToRobot, queue_size=5)
 
 
 
 def init_network():
-	ns = NetworkServer()
-
+	
 	rospy.init_node('virtual_network_server')
-	svc = rospy.Service('network_service', NetworkServer, ns.handle_msg_req)
+
+	#Get the maximum transmission range for the network
+	#Default value of -1 means any range is ok
+	maxTx = rospy.get_param("~txRange", -1.0)
+	ns = NetworkServer(maxTxRange = maxTx)
+
+	svc = rospy.Service('network_service', NetMsg, ns.handle_msg_req)
 	rospy.loginfo("Network service started...")
 
 	#Subscribe to april tag tag detections
-	rospy.Subscriber("/tag_detections", AprilTagDetectionArray, ds.update_tags)
+	rospy.Subscriber("/tag_detections", AprilTagDetectionArray, ns.update_tags)
 	rospy.spin()
 
 
 if __name__ == "__main__":
-	init_oracle()
+	init_network()
