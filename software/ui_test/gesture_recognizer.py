c5d06bdfb97d76765a63a6811577aea9083e0d15
Abe Shultz <orphrey@gmail.com>
2017-09-08 15:18:02 -0400

    Renders all the strokes from a given command in the same image

--- software/ui_test/gesture_recognizer.py
+++ software/ui_test/gesture_recognizer.py
@@ -21,7 +21,8 @@ import numpy as np
 import pickle
 import math
 
-infile = "26-08-17_17-10-15_s555_c10.pickle"
+#infile = "26-08-17_17-10-15_s555_c10.pickle"
+infile = "26-08-17_17_10_15_s555_c10.pickle"
 
 def isMeta(event):
 	if "desc" in event.keys():
@@ -42,17 +43,46 @@ def distance(x1, y1, x2, y2):
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
 
-#Debug function, dumps strokes to image files for viewing
-def dumpStroke(stroke):
-	pxlPad = 16
-	#Load parameters for the image, pad the width and height
-	path = "stroke_{0}.png".format(stroke.id)
-	width = int((stroke.maxX - stroke.minX)) + pxlPad
-	height = int((stroke.maxY - stroke.minY)) + pxlPad
+def dumpCommand(cmd):
+
+	#Generate a file name from the IDs of all the strokes in this command
+	fname = "{0}.png".format("_".join([str(x) for x in cmd.strokes.keys()]))
+
+	#Calculate the widths and heights
+	# minX = maxX = minY = maxY = 0
+	# for stroke in cmd.strokes.keys():
+	# 	minX = min(minX, cmd.strokes[stroke].minX)
+	# 	minY = min(minY, cmd.strokes[stroke].minY)
+	# 	maxX = max(maxX, cmd.strokes[stroke].maxX)
+	# 	maxY = max(maxY, cmd.strokes[stroke].maxY)
+
+	width = 1000 #int(maxX - minX)
+	height = 750 #int(maxY - minY)
 
 	#Create a new image with the required shape
-	img = Image.new("RGB", (width, height))
-	draw = ImageDraw.Draw(img)
+	image = Image.new("RGB", (width, height))
+	
+	for stroke in cmd.strokes.keys():
+		dumpStroke(cmd.strokes[stroke], fname, image)
+
+#Debug function, dumps strokes to image files for viewing
+def dumpStroke(stroke, fname=None, image=None):
+	pxlPad = 16
+	
+	#Load parameters for the image, pad the width and height
+	if fname == None:
+		fname = "stroke_{0}.png".format(stroke.id)
+
+	# If image is None, we're not dumping to an image that got created already
+	if image is None:
+		#Set up widths and heights for image
+		width = 1000 #int(stroke.width()) + pxlPad
+		height = 750 #int(stroke.height()) + pxlPad
+	
+		#Create a new image with the required shape
+		image = Image.new("RGB", (width, height))
+	
+	draw = ImageDraw.Draw(image)
 
 	#Set up the colors of the logged events by time
 	color_length = (len(stroke.events) + 3)/3
@@ -73,10 +103,10 @@ def dumpStroke(stroke):
 		#Convert to image coordinates
 		x = int(event['event_x'])
 		#This flips the image, kivy coordinates are upside-down relative to PIL coordinates
-		y = stroke.maxY - int(event['event_y'])
+		y = 750 - int(event['event_y'])
 		#Then move them from full screen space to the smaller image, including padding
-		x = x - (stroke.minX + pxlPad)/2
-		y = y - (stroke.minY + pxlPad)/2
+		#x = x - stroke.minX + (pxlPad/2)
+		#y = y - stroke.minY + (pxlPad/2)
 
 		#Draw the event
 		draw.ellipse([(x-3,y-3),(x+3,y+3)], fill=colors.pop(0))
@@ -85,16 +115,18 @@ def dumpStroke(stroke):
 	#Convert to image coordinates
 	x = int(stroke.centroid[0])
 	#This flips the image, kivy coordinates are upside-down relative to PIL coordinates
-	y = stroke.maxY - int(stroke.centroid[1])
+	y = 750 - int(stroke.centroid[1])
 	#Then move them from full screen space to the smaller image, including padding
-	x = x - stroke.minX + pxlPad/2
-	y = y - stroke.minY + pxlPad/2
+	#x = x - stroke.minX + pxlPad/2
+	#y = y - stroke.minY + pxlPad/2
 
 	#Draw an X
 	draw.line([(x-3,y-3),(x+3,y+3)], fill=(255, 0, 0))
 	draw.line([(x-3,y+3),(x+3,y-3)], fill=(255, 0, 0))
 
-	font = ImageFont.truetype("DejaVuSans.ttf", 16)
+	#font = ImageFont.truetype("DejaVuSansMono.ttf", 16)
+	#Specify font path precisely if just the file name doesn't work
+	font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/Arial.ttf", 10)
 		
 	#Can't have an angle between less than two events
 	if len(stroke.events) > 2:
@@ -113,21 +145,23 @@ def dumpStroke(stroke):
 		#This should be in radians, math.acos is in radians, according to the docs
 		#value is capped at 1 because numerical imprecision was causing math domain errors by feeding
 		#acos values like -1.00000000002, which is, technically, out of ranges
+		textLocation = (stroke.centroid[0] + 5, 750 - (stroke.centroid[1] - 5))
 		if (d1 * d2) == 0:
-			draw.text((0, 0),"d1 or d2 was zero",(255,255,255),font=font)
+			draw.text(textLocation,"d1 or d2 was zero",(255,255,255),font=font)
 			return
 		capped = clamp((pow(d1, 2) + pow(d2, 2) - pow(d3, 2))/(2 * d1 * d2), -1, 1)
 		angle = math.acos(capped)
 		if angle < 1:
-			draw.text((0, 0), "Circle {0} points".format(len(stroke.events)) ,(255,255,255),font=font)
+			draw.text(textLocation, "Circle {0} points".format(len(stroke.events)) ,(255,255,255),font=font)
 		elif angle < 2.5:
-			draw.text((0, 0), "Arc {0} points".format(len(stroke.events)) ,(255,255,255),font=font)
+			draw.text(textLocation, "Arc {0} points".format(len(stroke.events)) ,(255,255,255),font=font)
 		else:
-			draw.text((0, 0), "Line {0} points".format(len(stroke.events)) ,(255,255,255),font=font)		
+			draw.text(textLocation, "Line {0} points".format(len(stroke.events)) ,(255,255,255),font=font)		
 	else:
-		draw.text((0, 0), "Point" ,(255,255,255),font=font)
+		textLocation = (stroke.centroid[0] + 5, 750-(stroke.centroid[1] - 5))
+		draw.text(textLocation, "Point" ,(255,255,255),font=font)
 	#Write the file
-	img.save(path)
+	image.save(fname)
 
 class GestureStroke():
 	def __init__(self, event):
@@ -181,7 +215,23 @@ class GestureStroke():
 			return True
 		return False
 
-strokes = {}
+	def width():
+		return self.maxX - self.minX
+
+	def height():
+		return self.maxY - self.minY
+
+class GestureCommand():
+	def __init__(self):
+		self.strokes = {}
+
+	#Expects a dictionary of strokes
+	def addStrokes(self, strokes):
+		print "adding {0}".format(strokes.keys())
+		self.strokes = strokes
+
+
+commands = []
 
 #Load the recorded strokes from the user interface
 with open(infile, 'r') as inputData:
@@ -191,11 +241,25 @@ with open(infile, 'r') as inputData:
 	
 	try:
 		event = pickle.load(inputData)
+		
+		#Build a dictionary of strokes 
+		strokes = {}
+		commands = []
+		gc = GestureCommand()
+
 		while event is not None:
 			if isMeta(event):
-				#Don't do anything, meta is stuff like file loading, and this deals with contacts
-				pass
-			else:
+				print event
+				if event['desc'].startswith("Advanced") or event['desc'].startswith("Quit"):
+					#This is the delimiter between events in the log file
+					#TODO for parsing in realtime, it would have to be some recognition of an end of the command
+					if strokes:
+						#Don't add anything if there aren't any
+						gc.addStrokes(strokes)
+						commands.append(gc) #May be setting myself up for copy problems here
+						gc = GestureCommand()
+						strokes = {}
+ 			else:
 				if event['uid'] not in strokes.keys():
 					#If this event is just starting, create a new entry for it in strokes
 					strokes[event['uid']] = GestureStroke(event)
@@ -209,8 +273,7 @@ with open(infile, 'r') as inputData:
 	
 
 #Now strokes contains every touch event series
-for stroke in strokes.values():
-
-	dumpStroke(stroke)
+for command in commands:
+	dumpCommand(command)
 
 
