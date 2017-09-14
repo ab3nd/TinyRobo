163858ff2148ff582cc0fb9a9883f3ba4ce3f31a
Abe Shultz <orphrey@gmail.com>
2017-09-14 12:49:10 -0400

    Doesn't get me fullscreen, but fullscreen seems buggy

--- software/real_ui/real_ui.py
+++ software/real_ui/real_ui.py
@@ -8,27 +8,15 @@ kivy.require('1.9.1')
 #This has to be before any other Kivy imports, or it fails quietly
 from kivy.config import Config
 Config.set('graphics', 'resizable', False)
-#Config.set('graphics', 'fullscreen', 'fake')
-Config.set('graphics', 'borderless', False)
+#Config.set('graphics', 'fullscreen', "auto") #Setting this to 1 leaves screen in wrong resolution
+#Config.set('graphics', 'borderless', False)
 from kivy.app import App
 
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image as kvImage
 from kivy.graphics.texture import Texture
-from kivy.clock import Clock
-from kivy.core.image import Image as kvCoreImg
 from kivy.uix.camera import Camera
-
-import rospy
-from std_msgs.msg import Header
-from sensor_msgs.msg import Image as rosImage
-from cv_bridge import CvBridge, CvBridgeError
-import io
-import cv2
-from array import array
-import mmap
-from os import fork
 
 #At the moment, just listens for the quit command
 class KeyboardListener(Widget):
