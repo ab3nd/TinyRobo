c9ecb915d8855576fa4a473ad24c6cdc0282d234
ams <orphrey@gmail.com>
2017-10-21 16:55:53 -0400

    ungarbaged the transforms, but this is maybe not the most reliable tag tracker

--- software/catkin_ws/src/fiducial_transform_test/src/transform_points.py
+++ software/catkin_ws/src/fiducial_transform_test/src/transform_points.py
@@ -39,35 +39,65 @@ class Transformer(object):
 				"robot_id_{0}".format(robot_trans.fiducial_id),
 				tfMsg.header.frame_id)
 
+			#What if we draw a box at the transform location
+			marker = Marker()
+			marker.header.frame_id = tfMsg.header.frame_id
+			#"robot_id_{0}".format(robot_trans.fiducial_id)
+			#vtxMsg.header.frame_id
+			marker.header.stamp = rospy.Time.now()
+			marker.ns = "does_this_do_anything"
+			marker.id = robot_trans.fiducial_id
+			marker.type = Marker.CUBE
+			marker.action = Marker.ADD
+			marker.pose.position.x = translation[0]
+			marker.pose.position.y = translation[1]
+			marker.pose.position.z = translation[2]
+			marker.pose.orientation.x = quaternion[0]
+			marker.pose.orientation.y = quaternion[1]
+			marker.pose.orientation.z = quaternion[2]
+			marker.pose.orientation.w = quaternion[3]
+			marker.scale.x = 0.3
+			marker.scale.y = 0.3
+			marker.scale.z = 0.3
+			marker.color.a = 1.0 # Don't forget to set the alpha!
+			marker.color.r = 0.0
+			marker.color.g = 1.0
+			marker.color.b = 0.0
+			marker.lifetime = rospy.Time(10)
+			self.markerPub.publish(marker)
+
 	def update_points(self, vtxMsg):
 		#Rebroadcast all the verticies as ROS markers
 		#rospy.logwarn("Got verticies")
-		for tag in vtxMsg.fiducials:
-			points = [(tag.x0, tag.y0),(tag.x1, tag.y1),(tag.x2, tag.y2),(tag.x3, tag.y3)]
-			for point in points:
-				marker = Marker()
-				marker.header.frame_id = "robot_id_{0}".format(tag.fiducial_id)#vtxMsg.header.frame_id
-				marker.header.stamp = rospy.Time.now()
-				marker.ns = "does_this_do_anything"
-				marker.id = tag.fiducial_id
-				marker.type = Marker.SPHERE
-				marker.action = Marker.ADD
-				marker.pose.position.x = point[0]
-				marker.pose.position.y = point[1]
-				marker.pose.position.z = 0
-				marker.pose.orientation.x = 0.0
-				marker.pose.orientation.y = 0.0
-				marker.pose.orientation.z = 0.0
-				marker.pose.orientation.w = 1.0
-				marker.scale.x = 3
-				marker.scale.y = 3
-				marker.scale.z = 3
-				marker.color.a = 1.0 # Don't forget to set the alpha!
-				marker.color.r = 0.0
-				marker.color.g = 1.0
-				marker.color.b = 0.0
-				marker.lifetime = rospy.Time(10)
-				self.markerPub.publish(marker)
+		pass
+		# for tag in vtxMsg.fiducials:
+		# 	points = [(tag.x0, tag.y0),(tag.x1, tag.y1),(tag.x2, tag.y2),(tag.x3, tag.y3)]
+		# 	for point in points:
+		# 		marker = Marker()
+		# 		marker.header.frame_id = 
+		# 		#"robot_id_{0}".format(tag.fiducial_id)
+		# 		#vtxMsg.header.frame_id
+		# 		marker.header.stamp = rospy.Time.now()
+		# 		marker.ns = "does_this_do_anything"
+		# 		marker.id = tag.fiducial_id
+		# 		marker.type = Marker.SPHERE
+		# 		marker.action = Marker.ADD
+		# 		marker.pose.position.x = point[0]
+		# 		marker.pose.position.y = point[1]
+		# 		marker.pose.position.z = 0
+		# 		marker.pose.orientation.x = 0.0
+		# 		marker.pose.orientation.y = 0.0
+		# 		marker.pose.orientation.z = 0.0
+		# 		marker.pose.orientation.w = 1.0
+		# 		marker.scale.x = 3
+		# 		marker.scale.y = 3
+		# 		marker.scale.z = 3
+		# 		marker.color.a = 1.0 # Don't forget to set the alpha!
+		# 		marker.color.r = 0.0
+		# 		marker.color.g = 1.0
+		# 		marker.color.b = 0.0
+		# 		marker.lifetime = rospy.Time(10)
+		# 		self.markerPub.publish(marker)
 
 #Not a lot to do here
 my_xfmr = Transformer()
