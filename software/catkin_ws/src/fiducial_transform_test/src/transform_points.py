0e060cc6e67af3cc3d73aa3b7ad98bec839b5cbd
ams <orphrey@gmail.com>
2017-10-21 16:02:37 -0400

    progress on fixing build, transformer now runs (but may be bogus)

--- software/catkin_ws/src/fiducial_transform_test/src/transform_points.py
+++ software/catkin_ws/src/fiducial_transform_test/src/transform_points.py
@@ -1,4 +1,4 @@
-!#/usr/bin/python
+#!/usr/bin/python
 
 #Test getting points from the aruco fiducial tracker and transforming them to points in the real world
 #Also a bit of practice with TF in python
@@ -13,32 +13,40 @@ class Transformer(object):
 
 	def __init__(self):
 		#Set up publishers, subscribers, and such
-		rospy.initNode('transformPointsTest', anonymous=True)
+		#rospy.logwarn("Starting init")
+		rospy.init_node('transformPointsTest', anonymous=True)
 		self.markerPub = rospy.Publisher("vertex_marker", Marker, queue_size=10)
-		self.tfSub = rospy.Subscriber("/fiducial_transforms", FiducialArray, self.update_transforms)
-		self.pointSub = rospy.Subscriber("/fiducial_vertices", FiducialTransformArray, self.update_points)
+		self.tfSub = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.update_transforms)
+		self.pointSub = rospy.Subscriber("/fiducial_vertices", FiducialArray, self.update_points)
 		self.tfBcast = tf.TransformBroadcaster()
+		rospy.logwarn("Done with init")
 
 	def update_transforms(self, tfMsg):
 		#Rebroadcast all of the transforms as TF messages
+		#rospy.logwarn("Got transforms")
 		for robot_trans in tfMsg.transforms:
-			self.tfBcast(robot_trans.transform.translation, 
-				robot_trans.transform.translation,
+			translation = [robot_trans.transform.translation.x,
+						robot_trans.transform.translation.y,
+						robot_trans.transform.translation.z]
+
+			quaternion = [robot_trans.transform.rotation.x,
+						robot_trans.transform.rotation.y,
+						robot_trans.transform.rotation.z,
+						robot_trans.transform.rotation.w]
+			self.tfBcast.sendTransform(translation, 
+				quaternion,
 				rospy.Time.now(),
 				"robot_id_{0}".format(robot_trans.fiducial_id),
 				tfMsg.header.frame_id)
 
-			tmp.parent_id = tfMsg.header.frame_id
-			tmp.header.stamp = rospy.Time.now()
-			tmp.header.frame_id = "robot_id_{0}".format(self.t)
-
 	def update_points(self, vtxMsg):
 		#Rebroadcast all the verticies as ROS markers
+		rospy.logwarn("Got verticies")
 		for tag in vtxMsg.fiducials:
 			points = [(tag.x0, tag.y0),(tag.x1, tag.y1),(tag.x2, tag.y2),(tag.x3, tag.y3)]
 			for point in points:
 				marker = Marker()
-				marker.header.frame_id = vtxMsg.frame_id
+				marker.header.frame_id = vtxMsg.header.frame_id
 				marker.header.stamp = rospy.Time.now()
 				marker.ns = "does_this_do_anything"
 				marker.id = tag.fiducial_id
@@ -61,5 +69,5 @@ class Transformer(object):
 				self.markerPub.publish(marker)
 
 #Not a lot to do here
-tf = Transformer()
+my_xfmr = Transformer()
 rospy.spin()
