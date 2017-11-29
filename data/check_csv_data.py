b6bb820bb259f977c5e1b338e600f0843272d056
Abe Shultz <orphrey@gmail.com>
2017-11-29 16:38:03 -0500

    corrected missing commas and added ability to detect that to cleaning script

--- data/check_csv_data.py
+++ data/check_csv_data.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 
+import os 
+import csv
 #Go through all the data files and suggest validation/normalization stuff
 
 #Get all csv files in the directory
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-	for f in files:
-		import pdb; pdb.set_trace()
+files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv")]
+for f in files:
+	with open(f,"r") as infile:
+		csvFile = csv.DictReader(infile,delimiter=',')
+		for line in csvFile:
+			#Check that each field has a value, and that all the values are there
+			for key in line.keys():
+				if line[key] is None:
+					print "p{0}.csv timestamp {1} has None for {2}".format(line["user"], line["time"], key)
 
 #For each file, read the file
 
