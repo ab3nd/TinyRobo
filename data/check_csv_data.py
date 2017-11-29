2e263eaaae9cf4ed5754516721e3e24a82344031
Abe Shultz <orphrey@gmail.com>
2017-11-29 17:02:45 -0500

    removes fat-fingered timestamps that indicate time moving backwards

--- data/check_csv_data.py
+++ data/check_csv_data.py
@@ -4,22 +4,45 @@ import os
 import csv
 #Go through all the data files and suggest validation/normalization stuff
 
+
+#Store counts of all of the codes
+all_codes={}
+
+def add_code(code):
+	if code in all_codes.keys():
+		all_codes[code] += 1
+	else:
+		all_codes[code] = 1
+
 #Get all csv files in the directory
 files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv")]
 for f in files:
 	with open(f,"r") as infile:
 		csvFile = csv.DictReader(infile,delimiter=',')
+
+		#Used to check if timestamps are monotonically increasing
+		lastLineStamp = None
+
 		for line in csvFile:
 			#Check that each field has a value, and that all the values are there
 			for key in line.keys():
 				if line[key] is None:
 					print "p{0}.csv timestamp {1} has None for {2}".format(line["user"], line["time"], key)
+			
+			#Add the code to the count
+			add_code(line["event"])
 
-#For each file, read the file
-
-#Check that each line has the same number of commmas
-
-#Check that timestamps are monotonically increasing
+			#Check that timestamps are monotonically increasing
+			if lastLineStamp is None:
+				lastLineStamp = float(line["time"])
+			else:
+				currentStamp = float(line["time"])
+				if lastLineStamp < currentStamp:
+					#It's legit, time has advanced
+					lastLineStamp = currentStamp
+				else:
+					#It's not legit, time has flowed in reverse
+					print "p{0}.csv timestamp {1} exhibits retrochronality".format(line["user"], line["time"])
 
 #list all unique codes
 
