7fdbc9aa9c407078e4ec9a23c035d558512f3d96
Abe Shultz <orphrey@gmail.com>
2018-01-15 14:25:17 -0500

    added p 17

--- data/pretty_json.py
+++ data/pretty_json.py
@@ -9,7 +9,12 @@ jsonfiles = [x for x in os.listdir("./") if x.endswith(".json")]
 for file in jsonfiles:
 	data = None
 	with open(file, 'r') as infile:
-		data = json.loads(infile.read())
+		try:
+			data = json.loads(infile.read())
+		except ValueError:
+			print "Value error loading {0}".format(file)
+			raise
+			
 	with open(file, 'w') as outfile:
 		outfile.write(json.dumps(data, sort_keys=True, indent=3))
 
