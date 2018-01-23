407377b8bd5b8d39dac5868573a35533cb536506
ams <orphrey@gmail.com>
2018-01-23 14:33:08 -0500

    doesn't quite work, kivy is kind of short on error messages

--- software/catkin_ws/src/user_interface/src/test_kivy_image.py
+++ software/catkin_ws/src/user_interface/src/test_kivy_image.py
@@ -21,73 +21,104 @@ from kivy.uix.button import Button
 from kivy.graphics import Rectangle
 from array import array
 
+#From the Kivy example this is based on, remove later
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics import Color, Rectangle
+from random import random as r
+from functools import partial
+
+
+#For test, remove later
+from PIL import Image as PILImage
+
 import numpy as np
 from io import BytesIO  
 
 import cv2
 
-def makeImage():
-    
-    width = 32
-    height = 32
-    # create a 64x64 texture, defaults to rgb / ubyte
-    texture = Texture.create(size=(width, height), colorfmt='rgb')
-
-    # create 64x64 rgb tab, and fill with values from 0 to 255
-    # we'll have a gradient from black to white
-    size = width * height * 3
-    buf = [int(x * 255 / size) for x in range(size)]
-
-    # then, convert the array to a ubyte string
-    arr = array('B', buf)
-    # buf = b''.join(map(chr, buf))
-
-    # then blit the buffer
-    texture.blit_buffer(arr, colorfmt='rgb', bufferfmt='ubyte')
-    # return UIXImage(texture=tex, size=(width, height))
-
-    #return Button(text="WHY YOU NO RENDER")
-    #tmp_img = np.reshape(tmp_img, (width, height, 3))
-    #success, buf = cv2.imencode(".png", tmp_img)
-
-    #data = BytesIO(buf)
-
-    #import pdb; pdb.set_trace()
-    
-    #CoreImage rather than UIX image
-    #im = CoreImage(data, ext=".png")
-    
-    #Create a UIX image from the core image
-    #return UIXImage(texture = im.texture)
-
-    #This route leads to a lot of segfaults
-    #Convert it to a Kivy Texture
-    # tex = Texture.create(size=(width, height), colorfmt="rgb", bufferfmt='ubyte')
-    # tex.blit_buffer(byte_array.getvalue(), colorfmt='rgb', bufferfmt='ubyte')
-
-    # #Texture a widget with the texture and return that
-    # return UIXImage(texture=tex)
-    img = UIXImage()
-    with img.canvas:
-        Rectangle(texture=texture, pos=(0, 0), size=(width*3, height*3))
-
-    return img
-    #return UIXImage(source="./32_33_34_35_36_28_29_31.png")
+# rospy for the subscriber
+import rospy
+# ROS Image message
+from sensor_msgs.msg import Image
 
 class StupidApp(App):
 
     def __init__(self, **kwargs):
         super(StupidApp, self).__init__(**kwargs)
 
+        #intialize ROS and subscribe to an image topic
+        topic = "/overhead_cam/image_rect_color"
+        rospy.init_node('kivy_img_mauler')
+        #We can get away with not calling rospy.Spin() because Kivy keeps it running
+        self.sub = rospy.Subscriber(topic, Image, self.update_image)
+
+    def add_rects(self, label, wid, count, *largs):
+        label.text = str(int(label.text) + count)
+        with wid.canvas:
+            for x in range(count):
+                Color(r(), 1, 1, mode='hsv')
+                Rectangle(pos=(r() * wid.width + wid.x,
+                               r() * wid.height + wid.y), size=(20, 20))
+
+    def double_rects(self, label, wid, *largs):
+        count = int(label.text)
+        self.add_rects(label, wid, count, *largs)
+
+    def reset_rects(self, label, wid, *largs):
+        label.text = '0'
+        wid.canvas.clear()
+
     def build(self):
-        return makeImage()
-        #root.canvas.ask_update() 
+        wid = Widget()
+
+        label = Label(text='0')
+
+        btn_add100 = Button(text='+ 100 rects',
+                            on_press=partial(self.add_rects, label, wid, 100))
+
+        btn_add500 = Button(text='+ 500 rects',
+                            on_press=partial(self.add_rects, label, wid, 500))
+
+        btn_double = Button(text='x 2',
+                            on_press=partial(self.double_rects, label, wid))
+
+        btn_reset = Button(text='Reset',
+                           on_press=partial(self.reset_rects, label, wid))
+
+        layout = BoxLayout(size_hint=(1, None), height=50)
+        layout.add_widget(btn_add100)
+        layout.add_widget(btn_add500)
+        layout.add_widget(btn_double)
+        layout.add_widget(btn_reset)
+        layout.add_widget(label)
+
+        root = BoxLayout(orientation='vertical')
+        root.add_widget(wid)
+        root.add_widget(layout)
+
+        return root
         
     def on_pause(self):
         return True
 
     def on_stop(self):
         return True
+
+    def update_image(self, imgMsg):
+        file = io.BytesIO()
+        print "About to do the other thing!"
+        image = PILImage.new('RGBA', size=(50, 50), color=(155, 255, 0))
+        image.save(file, 'png')
+        file.name = 'test.png'
+        im = CoreImage(file, ext="png")
+
+        #Put it into the canvas
+        wid.add(Rectangle(size=(50,50), texture=im.texture))
+
+        print "Did the thing!"
+
 
 if __name__ == '__main__':
     StupidApp().run()
