33488cc842b37ce0663cb2e3bc22ba8d0fdec4a8
ams <orphrey@gmail.com>
2018-01-24 19:46:40 -0500

    moved to proper package

---
+++ software/catkin_ws/src/user_interface/src/destutter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+#ROS wrapper for gesture stroke consolidation, mostly getting gestures that are broken up 
+#by stuttering contact with the touchscreen into single sets of events
+
+import rospy
+from geometry_msgs.msg import PointStamped
+
+import touch_cleaner
+
+#Subscribe to the point messages from the UI 
+
+def touch_event_callback(event):
+	print "called"
+
+topic = "/touches"
+rospy.init_node('touch_destutter')
+touchSub = rospy.Subscriber(topic, PointStamped, touch_event_callback)
