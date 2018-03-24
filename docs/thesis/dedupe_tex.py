25ef6470cbdbcf2fa5d8a9c2c37bc2e6aa81ed19
ams <orphrey@gmail.com>
2018-03-24 19:27:00 -0400

    commented out duplicate lines

--- docs/thesis/dedupe_tex.py
+++ docs/thesis/dedupe_tex.py
@@ -3,10 +3,22 @@
 #Finds duplicate lines in a LaTeX file and comments out the second one
 
 import sys
+import re
 
 #No need for fancy command line processing
 infile = sys.argv[1]
 
+seenLines = {}
+
+
 with open(infile, 'r') as inputFile:
 	for line in inputFile:
-		print line
+		if line in seenLines.keys() and re.search("\S", line) :
+			#We've seen this line before
+			print "%", line,
+		elif re.search("cite{", line):
+			#Bad/old cites
+			print "%", line,
+		else:
+			seenLines[line] = 1
+			print line,
