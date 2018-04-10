6c8021088193c7c6d907cef75d08ad1d5e261674
ams <orphrey@gmail.com>
2018-04-10 14:11:08 -0400

    draws square corners

--- software/catkin_ws/src/compiler/src/decompose_space.py
+++ software/catkin_ws/src/compiler/src/decompose_space.py
@@ -14,10 +14,8 @@ points = [(-3.5,-1.1),(-2.3,0.0),(-1.2,0.2),(0.0,0.2),(3.0,0.2),(3.5,1.0)]
 #Bottom left and upper right corners of space
 space = [(-4,-2), (4,2)]
 
-def to_pygame(r, width = 640, height = 480):
+def to_pygame(r, width = 640, height = 480, ppm = 70):
 	
-	#Conversion of pixels to meters
-	ppm = 70
 
 	#Get the width and height of the rectangle in pixels
 	rect_w = (abs(r[0][0]) + abs(r[1][0])) * ppm
@@ -33,7 +31,23 @@ def to_pygame(r, width = 640, height = 480):
 	
 	return pygame.Rect(tl_x, tl_y, rect_w, rect_h)
 
-def pg_dbg(space):
+def point_to_pygame(pt, width = 640, height = 480, ppm = 70):
+	#Get the top left corner in pixels
+	tl_x = pt[0] * ppm
+	tl_y = -pt[1] * ppm # negate because increasing y moves down the screen, not up
+
+	#Convert to offset from center
+	tl_x += width/2
+	tl_y += height/2
+
+	tl_x = int(tl_x)
+	tl_y = int(tl_y)
+
+	print tl_x, tl_y
+
+	return (tl_x, tl_y)
+
+def pg_dbg(space, decomp):
 	pygame.init()
 	size = width,height = 640,480
 	screen = pygame.display.set_mode(size)
@@ -42,7 +56,12 @@ def pg_dbg(space):
 	#Draw the space
 	green = (0,128,0)
 	#Pygame rects are left, top, width, height
-	spc = pygame.draw.rect(screen, green, to_pygame(space) ,0)
+	pygame.draw.rect(screen, green, to_pygame(space) ,0)
+
+	#Draw a point for each decomp coordinate
+	for sq in decomp:
+		ppt = point_to_pygame(sq.tl)
+		pygame.draw.rect(screen, (0,200,0), pygame.Rect(ppt[0], ppt[1], 2, 2), 0) 
 	#center = pygame.draw.rect(screen, (150, 0, 0), pygame.Rect(width/2 - 5, height/2 - 5, 10,10),0)
 	pygame.display.flip()
 
@@ -53,21 +72,26 @@ def pg_dbg(space):
 
 
 class grid_sq(object):
-	def __init__(self):
-		self.tl = [0,0]
-		self.br = [0,0]
-		self.heading = 0
+	def __init__(self, tl = [0,0], br = [0,0], heading = 0):
+		self.tl = tl
+		self.br = br
+		self.heading = heading
 
 if __name__=="__main__":
 	
-	resolution = 0.05
-	space_w = (abs(r[0][0]) + abs(r[1][0]))
-	space_h = (abs(r[0][1]) + abs(r[1][1]))
-	x_coords = np.linspace(space[0][0], space[1][0], space_w/resolution)
-	y_coords = np.linspace(space[0][1], space[1][1], space_h/resolution)
+	resolution = 0.25
+	space_w = (abs(space[0][0]) + abs(space[1][0]))
+	space_h = (abs(space[0][1]) + abs(space[1][1]))
+	width = space_w/resolution
+	height = space_h/resolution
 
+	x_coords = np.linspace(space[0][0], space[1][0], width)
+	y_coords = np.linspace(space[0][1], space[1][1], height)
+
+	decomp = []
 	for x in x_coords:
 		for y in y_coords:
-			print "({},{})".format(x,y)
+			#print "({}, {})".format(x,y)
+			decomp.append(grid_sq([x, y], [x + width, y + height], 0))
 
-	pg_dbg(space)
+	pg_dbg(space, decomp)
