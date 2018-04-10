6f17b316f20d5b0627e6f2ca1b935e7c0ad5244e
ams <orphrey@gmail.com>
2018-04-10 16:46:33 -0400

    now with fixed tl and br corners

--- software/catkin_ws/src/compiler/src/decompose_space.py
+++ software/catkin_ws/src/compiler/src/decompose_space.py
@@ -10,11 +10,8 @@ import numpy as np
 import random
 import math
 
-#Points on the path in the space
-points = [(-3.5,-1.1),(-2.3,0.0),(-1.2,0.2),(0.0,0.2),(3.0,0.2),(3.5,1.0)]
-
-#Bottom left and upper right corners of space
-space = [(-4,-2), (4,2)]
+#tl and br corners of space
+space = [(-4,2),(4,-2)]
 
 def to_pygame(r, width = 640, height = 480, ppm = 70):
 	
@@ -25,7 +22,7 @@ def to_pygame(r, width = 640, height = 480, ppm = 70):
 
 	#Get the top left corner in pixels
 	tl_x = r[0][0] * ppm
-	tl_y = -r[1][1] * ppm # negate because increasing y moves down the screen, not up
+	tl_y = r[1][1] * ppm # negate because increasing y moves down the screen, not up
 
 	#Convert to offset from center
 	tl_x += width/2
@@ -47,7 +44,7 @@ def point_to_pygame(pt, width = 640, height = 480, ppm = 70):
 
 	return (tl_x, tl_y)
 
-def pg_dbg(space, decomp):
+def pg_dbg(space, decomp, points):
 	pygame.init()
 	size = width,height = 640,480
 	screen = pygame.display.set_mode(size)
@@ -58,26 +55,33 @@ def pg_dbg(space, decomp):
 	#Pygame rects are left, top, width, height
 	pygame.draw.rect(screen, green, to_pygame(space) ,0)
 
-	#Draw a point for each decomp coordinate
+	#Draw a vector for each decomp grid square
 	for sq in decomp:
-		#ppt = point_to_pygame(sq.tl)
-		#pygame.draw.rect(screen, (0,200,0), pygame.Rect(ppt[0], ppt[1], 2, 2), 0)
-
+		
 		#Get the center point
-		center = (sq.tl[0] + sq.width/2, sq.tl[1] - sq.height/2)
+		center = (sq.tl[0] + sq.width/2, sq.tl[1] + sq.height/2)
 		pg_center = point_to_pygame(center)
 
 		#Get a point that's half the width away on the heading
 		hx = (sq.tl[0] + sq.width/2) + (sq.width/2 * math.cos(sq.heading))
-		hy = (sq.tl[1] - sq.height/2) + (sq.width/2 * math.sin(sq.heading))
+		hy = (sq.tl[1] + sq.height/2) + (sq.width/2 * math.sin(sq.heading))
 		pg_vector = point_to_pygame([hx, hy])
 
-		print hx, hy
-		pygame.draw.line(screen, (0,0,140), (pg_center[0], pg_center[1]), (pg_vector[0], pg_vector[1]), 2)
-		#print pg_center
-		#Draw a point in the middle of each grid square
-		#pygame.draw.rect(screen, (0,0,140), pygame.Rect(pg_center[0], pg_center[1], 2, 2), 0) 
-	#center = pygame.draw.rect(screen, (150, 0, 0), pygame.Rect(width/2 - 5, height/2 - 5, 10,10),0)
+
+		#Conditional coloring of vectors
+		color = (0,200,0)
+		if sq.isAssigned:
+			color = (100, 100, 0)
+
+		pygame.draw.line(screen, color, (pg_center[0], pg_center[1]), (pg_vector[0], pg_vector[1]), 2)
+		pygame.draw.rect(screen, (100,100,0), pygame.Rect(pg_center[0], pg_center[1], 2, 2))
+	
+
+	#Draw all the points
+	for point in points:
+		ppt = point_to_pygame(point)
+		pygame.draw.circle(screen, (0,0,180), (ppt[0], ppt[1]), 2, 1)
+
 	pygame.display.flip()
 
 	while True:
@@ -86,6 +90,19 @@ def pg_dbg(space, decomp):
 				pygame.quit(); sys.exit();
 
 
+def isIn(point, sq):
+	print sq.br[0], "<", point[0], sq.br[0] < point[0]
+	print sq.tl[0], ">=", point[0], sq.tl[0] >= point[0]
+	print sq.br[1], "<", point[1], sq.br[1] < point[1]
+	print sq.tl[1], ">=", point[1], sq.tl[1] >= point[1]
+	print "==="
+	if (sq.br[0] <= point[0] and point[0] < sq.tl[0]) and (sq.br[1] <= point[1] and point[1] < sq.tl[1]):
+		return True
+	return False
+
+def isBetween(pointA, pointB, sq):
+	return True
+
 class grid_sq(object):
 	def __init__(self, tl = [0,0], br = [0,0], heading = 0):
 		self.tl = tl
@@ -93,6 +110,11 @@ class grid_sq(object):
 		self.width = abs(tl[0] - br[0])
 		self.height = abs(tl[1] - br[1])
 		self.heading = heading
+		self.isAssigned = False
+
+	def assign(self, heading):
+		self.heading = heading
+		self.isAssigned = True
 
 if __name__=="__main__":
 	
@@ -109,10 +131,25 @@ if __name__=="__main__":
 	x_coords = np.linspace(space[0][0], space[1][0], width_c)
 	y_coords = np.linspace(space[0][1], space[1][1], height_c)
 
-	decomp = []
-	for x in x_coords[:-1]:
-		for y in y_coords[1:]:
-			#Random headings to test rendering
-			decomp.append(grid_sq([x, y], [x + width, y + height], math.pi * (random.random() - 0.5)))
+	#Points on the path in the space
+	points = [(-3.5,-1.1),(-2.3,0.0),(-1.2,0.2),(0.0,0.2),(3.0,0.2),(3.5,1.0)]
 
-	pg_dbg(space, decomp)
+	decomp = []
+	for x in x_coords[1:]:
+		for y in y_coords[:-1]:
+			sq = grid_sq([x - width, y - height], [x, y], math.pi * (random.random() - 0.5))
+
+			for pointIdx in range(len(points)-1):
+				if isIn(points[pointIdx], sq):
+					#Heading of each point is towards next point
+					p1 = points[pointIdx]
+					p2 = points[pointIdx]
+					sq.assign(math.atan2(p2[1]-p1[1], p1[0]-p2[0]))
+
+
+			#Random headings to test rendering
+			decomp.append(sq)
+
+
+
+	pg_dbg(space, decomp, points)
