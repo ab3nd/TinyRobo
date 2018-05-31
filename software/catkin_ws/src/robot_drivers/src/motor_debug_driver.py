953915ea46ebe25a437e2413d82136b83f07a353
ams <orphrey@gmail.com>
2018-05-31 18:13:45 -0400

    Trying to solve projection of clicked points into camera frame

--- software/catkin_ws/src/robot_drivers/src/motor_debug_driver.py
+++ software/catkin_ws/src/robot_drivers/src/motor_debug_driver.py
@@ -2,13 +2,23 @@
 
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PointStamped
 from apriltags_ros.msg import *
 import random
 import math
 from tf import transformations as trans
-from geometry_msgs.msg import PointStamped
+
 from tf import TransformListener
+from image_geometry import PinholeCameraModel
+from sensor_msgs.msg import CameraInfo
+from trianglesolver import solve
+
+def dot(a, b):
+	assert len(a) == len(b)
+	d = 0
+	for i, j in zip(a, b):
+		d += i*j
+	return d
 
 class Point_Driver():
 	def __init__(self):
@@ -21,6 +31,9 @@ class Point_Driver():
 
 		self.currentX = 0.0
 		self.currentY = 0.0
+		self.currentZ = 0.0
+		self.currentX_px = 0.0
+		self.currentY_px = 0.0
 
 		all_tags = rospy.get_param('/apriltag_detector/tag_descriptions')
 		self.tagsize = 0.051 #default, but we try to update it
@@ -31,6 +44,8 @@ class Point_Driver():
 		self.conversion = 0.0
 
 		self.tf = TransformListener()
+
+		self.camera_model = None
 
 	def update_tags(self, tags_msg):
 		#Get the location of this robot from the tag
@@ -51,34 +66,100 @@ class Point_Driver():
 
 		self.currentX = tag.pose.pose.position.x 
 		self.currentY = tag.pose.pose.position.y 
+		self.currentZ = tag.pose.pose.position.z 
+		self.currentX_px = tag.tagCenterPx.x
+		self.currentY_px = tag.tagCenterPx.y
+
+
+	def update_cam(self, cam_info):
+		self.camera_model = PinholeCameraModel()
+		self.camera_model.fromCameraInfo(cam_info)
 
 	def update_target(self, point_msg):
 		
 		#Kivy coordinates have (0,0) at bl of image/window
 		#Camera coordinates have (0,0) at center of image
 		#These magical numbers (the image size in meters) are in Very Poor Style
-		img_h_m = 768.0 / self.conversion
-		img_w_m = 1024.0 / self.conversion
-		
-		#Convert point in pixels to meters in app frame
-		targetPoint = PointStamped()
-		targetPoint.header.frame_id = "ui_app"
-		targetPoint.header.stamp = rospy.Time(0)
-		#Convert to meters and move origin
-		targetPoint.point.x = (point_msg.point.x / self.conversion) - (img_w_m/2)
-		targetPoint.point.y = (point_msg.point.y / self.conversion) - (img_h_m/2)
-		targetPoint.point.z = 0.0 #Don't care about 3d location?
+		#img_h_m = 768.0 / self.conversion
+		#img_w_m = 1024.0 / self.conversion
 
-		#Get a transform from the app to the camera frame
-		if self.tf.frameExists("ui_app") and self.tf.frameExists("camera_frame"):
-			targetPoint = self.tf.transformPoint("camera_frame", targetPoint)
+		#flip points on y axis because kivy (0,0) is bl, image (0,0) is tl
+		image_height = 768
+		px_x = point_msg.point.x
+		px_y = max(image_height-point_msg.point.y, 0)
 
 
-		self.targetX = targetPoint.point.x
-		self.targetY = targetPoint.point.y
+		#Figure out where the point is in camera space
+		if self.camera_model is not None:
+			#Unit vector pointing to the clicked point, in the camera frame (origin at center of camera)
+			ray_to_click = self.camera_model.projectPixelTo3dRay((px_x, px_y))
+			#Second point, needed to create a triangle to figure out the distance along the ray
+			ray_to_tag = self.camera_model.projectPixelTo3dRay((self.currentX_px, self.currentY_px))
+			
+			#use trianglesolver to get the sides and angles I don't have
 
-		print self.conversion
-		print ("Current ({}, {}), Target ({}, {})").format(self.currentX, self.currentY, self.targetX, self.targetY)
+			#Get distance from camera to tag in meters (Euclidian distance from origin)
+			cam_to_tag = math.sqrt(math.pow(self.currentX, 2) + math.pow(self.currentY, 2) + math.pow(self.currentZ, 2))
+			#Get distance from tag center to clicked point in px, then convert to meters
+			tag_to_click = math.sqrt(math.pow(self.currentX_px - px_x, 2) + math.pow(self.currentY_px - px_y, 2))
+			tag_to_click = tag_to_click/self.conversion
+			#Get angle between rays to clicked point and tag in radians
+			#This is dot product in 3d, then take the arc cosine to get radians
+			cam_angle = (float(ray_to_click[0])*float(ray_to_tag[0])) + (float(ray_to_click[1])*float(ray_to_tag[1])) + (float(ray_to_click[2])*float(ray_to_tag[2]))
+			cam_angle = math.acos(cam_angle)
+
+			# Triangle is set up like this:
+			#
+			# 	   		   cam_angle
+			# 			     /  \
+			# 			    /    \
+			#   cam_to_tag /      \ cam_to_click (?)
+			#             /        \
+			#            /          \
+			#    tag_angle (?)----click_angle (?)
+			#                   |
+			#              tag_to_click
+            #
+   			# The values with a question mark are initially unknown
+
+   			#The triangle solver needs more information to disambiguate triangles, so find tag angle
+   			#The ray to the camera points to the origin, so it's just the tag location, normalized to a unit vector
+   			# mag = math.sqrt(math.pow(self.currentX_px, 2) + math.pow(self.currentY_px, 2) + math.pow(self.currentZ/self.conversion,2))
+   			# ray_to_cam = (self.currentX_px/mag, self.currentY_px/mag, (self.currentZ/self.conversion)/mag)
+   			# #The ray to the click is the click location minus the tag location, the Z values are the same,
+   			# #so the result of subtracting them is 0
+   			# mag = math.sqrt(math.pow(self.currentX_px - px_x, 2) + math.pow(self.currentY_px - px_y, 2))
+   			# ray_to_click_from_tag = (self.currentX_px - px_x, self.currentY_px - px_y, 0)
+
+   			# tag_angle = math.acos(dot(ray_to_cam, ray_to_click_from_tag))
+   			# print dot(ray_to_cam, ray_to_click_from_tag), tag_angle
+
+
+			#Solve for the unknowns with trianglesolver (https://pypi.org/project/trianglesolver/)
+			cam_to_click, cam_to_click, tag_to_click, tag_angle, click_angle, cam_angle = solve(b=cam_to_tag, c=tag_to_click, C=cam_angle)
+
+			print cam_to_click, cam_to_click, tag_to_click, tag_angle, click_angle, cam_angle
+
+		print "Click ({}, {}), Current px ({}, {})".format(point_msg.point.x, point_msg.point.y, self.currentX_px, self.currentY_px)		
+		# #Convert point in pixels to meters in app frame
+		# targetPoint = PointStamped()
+		# targetPoint.header.frame_id = "ui_app"
+		# targetPoint.header.stamp = rospy.Time(0)
+		# #Convert to meters and move origin
+		# targetPoint.point.x = (point_msg.point.x / self.conversion) - (img_w_m/2)
+		# targetPoint.point.y = (point_msg.point.y / self.conversion) - (img_h_m/2)
+		# targetPoint.point.z = 0.0 #Don't care about 3d location?
+
+		# #Get a transform from the app to the camera frame
+		# if self.tf.frameExists("ui_app") and self.tf.frameExists("camera_frame"):
+		# 	targetPoint = self.tf.transformPoint("camera_frame", targetPoint)
+
+
+		# self.targetX = targetPoint.point.x
+		# self.targetY = targetPoint.point.y
+
+		# print self.conversion
+		# print ("Current ({}, {}), Target ({}, {})").format(self.currentX, self.currentY, self.targetX, self.targetY)
 
 		# #Calculate the error between the rotation of the robot and orientation towards the point
 		# x = self.targetX - tag.pose.pose.position.x 
@@ -144,6 +225,7 @@ if __name__ == '__main__':
 
 	go_sub = rospy.Subscriber('/touches', PointStamped, pd.update_target)
 
+	cam_sub = rospy.Subscriber('/overhead_cam/camera_info', CameraInfo, pd.update_cam)
 
 	rospy.spin()
 
