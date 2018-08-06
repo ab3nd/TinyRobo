0bd2f3e1e9f8db75aaf7b476a72996449d9760b0
ams <orphrey@gmail.com>
2018-08-06 16:20:25 -0400

    Don't do updates while being touched, does a much better job getting all touches

--- software/catkin_ws/src/user_interface/src/destutter.py
+++ software/catkin_ws/src/user_interface/src/destutter.py
@@ -22,39 +22,45 @@ class TouchCollector(object):
 			#Event already started, extend its entry
 			self.strokes[event.uid].addEvent(event)
 
-		#Process all of the events which have ended
+		#Get a list of all the ended strokes and see if any need to be merged
+		ended_strokes = []
 		for stroke_id in self.strokes.keys():
 			if self.strokes[stroke_id].isEnded:
-				print "Stroke {0} ended".format(stroke_id)
-				touch_cleaner.dumpStroke(self.strokes[stroke_id])
-				del self.strokes[stroke_id]
+				ended_strokes.append(stroke_id)
 
-# #Now strokes contains every touch event series
-# for command in commands:
-# 	dumpCommand(command)
+		#Stutter removal. For each stroke and the stroke after it, if they are seperated by less than 
+		#the stutter thresholds in both time and space, they are a product of a poor finger tracking over 
+		#the screen, not actually intended to be seperate events. This merges some strokes.
+		#TODO may need a spatial as well as chronological component for properly resolving multitouch membership
+		for index, item in enumerate(ended_strokes):
+			if index > 0: 
+				current = self.strokes[ended_strokes[index]]
+				prev = self.strokes[ended_strokes[index - 1]]
 
-#Do an all pairs comparison to figure out whether any of the strokes in each of the commands should be merged,
-# This is to determine what the time threshold is between commands that are deliberately 
-# seperate from each other, and commands that are the same, but seperated by finger stutter on the screen
-# for cmd in commands:
-# 	#Each command has strokes, each stroke is a canidate for merging with each other stroke
-# 	for strokeA in cmd.strokes.values():
-# 		for strokeB in cmd.strokes.values():
-# 			if strokeA.overlaps(strokeB):
-# 				#The events overlap. Either A starts first or B does
-# 				overlapTime = min(strokeA.endTime, strokeB.endTime) - max(strokeA.startTime, strokeB.startTime)
+				if prev.overlaps(current):
+					#The events overlap. Either the current one started first or the previous one did
+					overlapTime = min(prev.endTime, current.endTime) - max(prev.startTime, current.startTime)
 
-# 				if strokeA.startTime < strokeB.startTime:
-# 					print "{0} overlaps {1} by {2}".format(strokeA.id, strokeB.id, overlapTime)
-# 				else:
-# 					print "{0} overlaps {1} by {2}".format(strokeA.id, strokeB.id, overlapTime)
-# 			else:
-# 				#The events don't overlap
-# 				#TODO this doesn't need to be all pairs, I only care about sequences
-# 				if strokeA.endTime < strokeB.startTime:
-# 					if (strokeB.id - strokeA.id) == 1:
-# 						print "{0} ends {1} before {2} begins".format(strokeA.id, strokeB.startTime - strokeA.endTime, strokeB.id)
-# 	print "---"		
+					# if prev.startTime < current.startTime:
+					# 	print "{0} overlaps {1} by {2}".format(prev.id, current.id, overlapTime)
+					# else:
+					# 	print "{0} overlaps {1} by {2}".format(prev.id, current.id, overlapTime)
+					if overlapTime < 0.01:
+						current.merge(prev)
+						#TODO Is this going to give me a bad day? I'm not mutating the thing I'm enumerating...
+						del(self.strokes[ended_strokes[index-1]])
+				else:
+					#The events don't overlap
+					if prev.endTime < current.startTime:
+						#print "{0} ends {1} before {2} begins".format(prev.id, current.startTime - prev.endTime, current.id)
+						if current.startTime - prev.endTime < 0.099:
+							current.merge(prev)
+							del(self.strokes[ended_strokes[index-1]])
+		
+		#dump for debugging
+		for stroke_id in self.strokes.keys():		
+			touch_cleaner.dumpStroke(self.strokes[stroke_id])
+			del self.strokes[stroke_id]
 
 topic = "/touches"
 rospy.init_node('touch_destutter')
