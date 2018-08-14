6eed9e1218af6bc3af67d354111ea7fc0afb65a9
ams <orphrey@gmail.com>
2018-08-14 19:05:38 -0400

    adds lasso select detector, which has an implementation of minimum bounding ovals

--- software/catkin_ws/src/user_interface/src/box_select_detector.py
+++ software/catkin_ws/src/user_interface/src/box_select_detector.py
@@ -5,10 +5,8 @@ from user_interface.msg import Kivy_Event, Stroke
 from geometry_msgs.msg import Point
 import math
 from apriltags_ros.msg import *
+import classification_heuristics
 
-#For debugging
-import Image, ImageDraw, ImageFont
-import numpy as np
 
 # Receives the locations of the robots and the strokes made by the user, and determines if the 
 # resulting configuration could be a box selection. Generally, this means that the user drew a line
@@ -27,30 +25,30 @@ class BoxSelectDetector(object):
 			self.currentTags[int(msg.detections[ii].id)] = msg.detections[ii]
 
 	def check_stroke(self, msg):
-		#For debugging
-		#dumpStroke(msg)
-		
-		selected_tags = []
-		if len(self.currentTags) > 0:
-			#Get the bounding box of the stroke
-			xs = [event.point.x for event in msg.events]
-			minX = min(xs)
-			maxX = max(xs)
-			ys = [event.point.y for event in msg.events]
-			minY = min(ys)
-			maxY = max(ys)
-			#Get all the tags that have at least one corner in the box
-			for tag in self.currentTags.values():
-				# Rescale from pixels in camera view to pixels in UI view (cropped, embiggened image)
-				tag_x = tag.tagCenterPx.x * 1.640625
-				tag_y = (tag.tagCenterPx.y - 120) * 1.640625
+		#Only line gestures can be box selections
+		if classification_heuristics.is_line(msg):		
+			#Clear out selected tags and see if the new gesture selects any of them
+			selected_tags = []
+			if len(self.currentTags) > 0:
+				#Get the bounding box of the stroke
+				xs = [event.point.x for event in msg.events]
+				minX = min(xs)
+				maxX = max(xs)
+				ys = [event.point.y for event in msg.events]
+				minY = min(ys)
+				maxY = max(ys)
+				#Get all the tags that have at least one corner in the box
+				for tag in self.currentTags.values():
+					# Rescale from pixels in camera view to pixels in UI view (cropped, embiggened image)
+					tag_x = tag.tagCenterPx.x * 1.640625
+					tag_y = (tag.tagCenterPx.y - 120) * 1.640625
 
-				if (minX < tag_x < maxX) and (minY < tag_y < maxY):
-					selected_tags.append(tag.id)
+					if (minX < tag_x < maxX) and (minY < tag_y < maxY):
+						selected_tags.append(tag.id)
 
-			if len(selected_tags) > 0:
-				#This is possibly a box select, pack it up and publish it
-				rospy.loginfo("{0} selects {1}".format(msg.uid, selected_tags))
+				if len(selected_tags) > 0:
+					#This is possibly a box select, pack it up and publish it
+					rospy.loginfo("{0} selects {1}".format(msg.uid, selected_tags))
 		
 rospy.init_node('box_select_detect')
 bsd = BoxSelectDetector()
