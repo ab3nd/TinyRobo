abc3308d09bf65303e483aa92de78f2e44e09e91
Abe Shultz <orphrey@gmail.com>
2019-01-16 15:49:46 -0500

    pushing so I can go work on a 16.x ubuntu

--- software/catkin_ws/src/spoof_apriltags/src/spoof_pixels.py
+++ software/catkin_ws/src/spoof_apriltags/src/spoof_pixels.py
@@ -5,9 +5,12 @@
 import argparse
 import rospy
 from apriltags_ros.msg import *
+import cv2
 
 parser = argparse.ArgumentParser(description='Publish AprilTag messages based on the command line rather than actual tags.')
-parser.add_argument('tag_locations', metavar='N', type=float, nargs='+',
-                    help='values for tag positions')
+parser.add_argument('--img', dest='img_path', action='store', default="",
+                    help='path to image file to use')
 parser.add_argument('--rate', dest='pub_rate', action='store_const', default=10,
                     help='rate setting, default is 10Hz')
+
+
