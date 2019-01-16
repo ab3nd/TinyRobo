2d101c050fb6ecbe538d70433536d212628237b5
Abe Shultz <orphrey@gmail.com>
2019-01-16 15:11:59 -0500

    pixel-location only

---
+++ software/catkin_ws/src/spoof_apriltags/src/spoof_pixels.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#Generates spoof april tags based on the command line 
+
+import argparse
+import rospy
+from apriltags_ros.msg import *
+
+parser = argparse.ArgumentParser(description='Publish AprilTag messages based on the command line rather than actual tags.')
+parser.add_argument('tag_locations', metavar='N', type=float, nargs='+',
+                    help='values for tag positions')
+parser.add_argument('--rate', dest='pub_rate', action='store_const', default=10,
+                    help='rate setting, default is 10Hz')
