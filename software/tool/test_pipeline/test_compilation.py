979736aa14e6526fb33abe0e746c563457a8b730
ams <orphrey@gmail.com>
2019-02-05 02:05:21 -0500

    adding data from analysis, initial writeup

--- software/tool/test_pipeline/test_compilation.py
+++ software/tool/test_pipeline/test_compilation.py
@@ -13,13 +13,16 @@ import os
 
 
 class ProgGen(object):
-	def __init__(self):
+	def __init__(self, logpath = "test_log.text"):
 		self.gestureSub = rospy.Subscriber("gestures", Gesture, self.addGesture)
 		#All gestures seen so far, treated as a stack
 		self.gestures = []
 
-		#Open a log file
-		self.logfile=open("test_log.txt", "w")
+		self.throw_count = 0
+
+		#Open a log file for append, we tear this generator down each time 
+		#so there isn't saved state
+		self.logfile=open(logpath, "w")
 
 		#Implemented as per my thesis paper, only without CamelCase because lark is case sensitive
 		gesture_grammar='''
@@ -70,13 +73,14 @@ class ProgGen(object):
 		#This allows timeouts (sort-of, more than Nsec since the previous gesture), a "go" gesture,
 		#or firing off the previous command when selection begins a new command. 
 		self.checkGestureList()
+		
 
 
 	def parseGestures(self, gesture_list):
 		#Get a list of the gestures
 		prog_str = " ".join([g.eventName for g in gesture_list])
 		#Log it for debugging/testing
-		self.logfile.write("Program: {}\n".format(prog_str))
+		self.log("Program: {}\n".format(prog_str))
 		#Generate a code string from the gestures in the buffer
 		prog = []
 		for g in gesture_list:
@@ -105,38 +109,41 @@ class ProgGen(object):
 		parse_tree = self.parser.parse(prog_str)
 
 		#For now just prettyprint it
-		self.logfile.write(prog_str)
-		self.logfile.write("\n")
-		self.logfile.write(parse_tree.pretty())
-		self.logfile.write("\n")
-		self.logfile.write("---")
-		self.logfile.write("\n")
+		self.log(prog_str)
+		self.log("\n")
+		self.log(parse_tree.pretty())
+		self.log("\n")
+		self.log("---")
+		self.log("\n")
 		# for t in parse_tree.children:
 		#Don't bother, we just want to see if it parses at all
 		#self.handle_instruction(parse_tree)
 
 	def checkGestureList(self):
+		#rospy.logwarn("1. {} gestures in queue".format(len(self.gestures)))
 		#If the last thing in is an end gesture, we're good to try to parse the gestures
 		if self.gestures[-1].eventName == "end":
-			self.logfile.write("--> gesture ended\n")
-
+			self.log("--> gesture ended\n")
 			#Call the parser on it
+			#rospy.logwarn("1.2 {} gestures in queue".format(len(self.gestures)))
 			try:
 				#Parse it to build the tree
 				self.parseGestures(self.gestures)
 			except UnexpectedInput as e:
-					self.logfile.write(str(e))
-			finally:
-				#Flush the gesture buffer
-				self.gestures = []
+				self.log(str(e))
+			#Flush the gesture buffer
+			self.gestures = []
+			#rospy.logwarn("1.3 {} gestures in queue".format(len(self.gestures)))
 
 		#If the last thing in is a select, and there are any selects already present in the buffer,
 		#then it's time to end the previous gesture and parse that, and leave the select in as
 		#the beginning of a new gesture
 		elif self.isSelect(self.gestures[-1]) and any([self.isSelect(x) for x in self.gestures[:-1]]):
+			import pdb; pdb.set_trace()
 			#A second select is possible for a move gesture, so we can accept it in this case
 			if not any(x.eventName == 'move_object' for x in self.gestures):
-				self.logfile.write("--> select started new gesture\n")
+				#rospy.logwarn("2.1 {} gestures in queue".format(len(self.gestures)))
+				self.log("--> select started new gesture\n")
 				#insert an end to it and ship it to the parser
 				toParse = self.gestures[:-1]
 				evt = Gesture()
@@ -151,11 +158,12 @@ class ProgGen(object):
 				try:
 					self.parseGestures(toParse)
 				except UnexpectedInput as e:
-						self.logfile.write(str(e))
-				finally:			
-					#Clear the stuff that was just sent to the parser, but leave the 
-					#selection gesture that kicked off this parse attempt
-					self.gestures = [self.gestures[-1]]
+					self.log(str(e))
+				#Clear the stuff that was just sent to the parser, but leave the 
+				#selection gesture that kicked off this parse attempt
+				#rospy.logwarn("2.2 {} gestures in queue".format(len(self.gestures)))
+				self.gestures = [self.gestures[-1]]
+				#rospy.logwarn("2.3 {} gestures in queue".format(len(self.gestures)))
 
 	def isPath(self, gestureMsg):
 		if gestureMsg.eventName == "path":
@@ -176,8 +184,7 @@ class ProgGen(object):
 				resp = pm(p.point)
 				path.append("(" + str(resp.inMeters.x) + "," + str(resp.inMeters.y)+ ")")
 		return path
-		
-
+	
 	def get_start(self,g):
 		path = []
 		path.append("(" + str(g.strokes[0].events[0].point.x) + "," + str(g.strokes[0].events[0].point.y)+ ")")
@@ -186,7 +193,11 @@ class ProgGen(object):
 	def log(self, message):
 		self.logfile.write(message)
 		self.logfile.write("\n")
+		self.logfile.flush()
 
+	def flush(self):
+		self.logfile.flush()
+		
 #From https://stackoverflow.com/questions/1724693/find-a-file-in-python
 def find(pattern, path):
     result = []
@@ -200,38 +211,48 @@ def find(pattern, path):
 #Set up the mock generator
 rospy.init_node('mock_gcpr_gen')
 
-#This automatically subscribes to /gestures
-progam_generator = ProgGen()
-
 #The script can publish gestures too, so it can send end gestures
 gesturePub = rospy.Publisher("gestures", Gesture, queue_size=10)
 
 mapperP = subprocess.Popen(["rosrun", "robot_drivers", "sim_point_mapper_svc.py"])
 
 #Get the gesture files
-basepath = "./good_run/"
-gesture_files = find("*gestures.bag", basepath)
+# basepath = "./good_run/"
+# gesture_files = find("*gestures.bag", basepath)
 
-#For each gesture file
-for bag in gesture_files:
+#bags = sorted(gesture_files)
+#for bag in bags: # sorted(gesture_files):
 
-	progam_generator.log(bag)	
+bag = sys.argv[1]
 
-	#play the gesture bag file
-	rosbagP = subprocess.Popen(["rosbag", "play", bag])
-	rospy.loginfo("Playing bag, please wait")
+outfile = os.path.basename(bag).replace("gestures.bag", "output.txt")
 
-	rosbagP.wait()
+#This automatically subscribes to /gestures
+progam_generator = ProgGen(outfile)
 
-	#Send a double-tap
-	evt = Gesture()
-	evt.eventName = "end"
-	evt.stamp = rospy.Time.now()
-	evt.isButton = False 
-	evt.robots = []
-	evt.strokes = []
-	gesturePub.publish(evt)
+rospy.sleep(1)
 
+progam_generator.log("\n========================================\n" + bag)	
+
+#play the gesture bag file
+rosbagP = subprocess.Popen(["rosbag", "play", bag])
+rospy.loginfo("Playing bag, please wait")
+
+rosbagP.wait()
+
+#Send a double-tap
+evt = Gesture()
+evt.eventName = "end"
+evt.stamp = rospy.Time.now()
+evt.isButton = False 
+evt.robots = []
+evt.strokes = []
+gesturePub.publish(evt)
+
+rospy.sleep(3)
+
+progam_generator.log("\ndone.")
+	
 def myhook():
   mapperP.terminate()
   # rosbagP.terminate()
